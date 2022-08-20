09fd2486ea7fb422dae66282f8b4f42d6f393315
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-08-20 20:01:57 +0530

    change:logo

--- src/components/globals/Header.js
+++ src/components/globals/Header.js
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import Image from "../../images/janus-softwares.png";
+import Image from "../../images/Janus_Logo.png";
 
 const Header = (props) => {
   console.log(props);
